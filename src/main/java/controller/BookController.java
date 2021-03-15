72405d1f0c83bffa7d32e206a971576c64f41b59
Amin Oruji <oruji66@gmail.com>
2021-03-15 22:11:59 +0330

    add thymeleaf for books

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -1,9 +1,14 @@
 package controller;
 
+import domain.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import repository.BookRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BookController {
@@ -17,8 +22,23 @@ public class BookController {
     @RequestMapping("/books")
     public String getBooks(Model model) {
 
+        System.out.println("I'm Request map books");
+
         model.addAttribute("books", bookRepository.findAll());
 
-        return "books";
+        return "books/list";
     }
+
+//    @RequestMapping("/mybooks")
+//    public @ResponseBody
+//    List<Book> getBooks(Model model) {
+//        System.out.println("I'm Request map books");
+//
+//        List<Book> result = new ArrayList<>();
+//        for (Book str : bookRepository.findAll()) {
+//            result.add(str);
+//        }
+//
+//        return result;
+//    }
 }
